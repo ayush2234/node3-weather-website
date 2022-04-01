658bd02d4e657014655bd7101b0dacaa140ec92b
Ayush Agarwal <ayush980333@gmail.com>
2022-04-01 13:59:20 +0545

    Add temperature and humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require('request')
+
 const forecast= (address,callback) => { 
     const url ='https://samples.openweathermap.org/data/2.5/'+ address + '?id=524901&appid=b1b15e88fa797225412429c1c50c122a1'
     request({ url,json:true},(error, { body} ) => { 
@@ -8,8 +9,9 @@ const forecast= (address,callback) => {
                     callback('Unable to find location.Try another search.',undefined)
             
                  } else{ 
+                     
                     callback(undefined,{ 
-                        forecast:'It is currently ' + body.list[0].main.temp_max + ' degress out. There is a '+  body.list[0].main.pressure+ ' % chance of rain',
+                        forecast:'It is currently ' + body.list[0].main.temp_max + ' degress out. This tempresture is'+body.list[0].main.temp+ 'with a humidity'+ body.list[0].main.humidity+ '.There is a '+  body.list[0].main.pressure+ ' % chance of rain',
                         latitude: body.list[0].main.temp_max  ,
                         longitude: body.list[0].main.temp_max,
                         description: body.list[0].weather[0].description
