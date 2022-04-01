3d9fa3d96c6b07e620dd4fa44bb049095172035a
Ayush Agarwal <ayush980333@gmail.com>
2022-04-01 11:36:34 +0545

    Init commit

---
+++ src/app.js
@@ -0,0 +1,119 @@
+const path= require('path')
+const express= require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath= path.join(__dirname,'../public')
+const viewPath = path.join(__dirname,'../templates/views')
+const partials = path.join(__dirname,'../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partials)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+ app.get('', (req,res) => { 
+     res.render('index', { 
+         title:'Weather',
+         name: 'Ayush Agarwal'
+     })
+ })
+
+ app.get('/about', (req,res) => {
+     res.render('about', {
+         title: 'About me',
+         name: 'Ayush Agarwal'
+    })
+ })
+
+ app.get('/help', (req, res) => { 
+     res.render('help', { 
+         helpText: 'This is some helpful text.',
+         title: 'help',
+         name: 'Ayush Agarwal'
+     })
+ })
+// app.get('', (req, res) => { 
+//     res.send('<h1> Weather</h1>')
+// })
+
+// app.get('/help', (req,res) => { 
+//      res.send([{
+//          name: 'Ankit'
+//      },{
+//          name: 'Ayush',
+//          age: 26
+//      }])
+// } )
+
+// app.get('/about', (req, res) => { 
+//     res.send('<h1> About page</h1>')
+// })
+
+app.get('/weather', (req, res) => { 
+
+    if(!req.query.address){ 
+        return res.send ({
+            error: 'You must provide an address!'
+        })
+    }
+     forecast(req.query.address, (error, data) => { 
+         if(error) {
+             return res.send({error})
+         }
+
+         res.send({
+             forecast: data,
+             address: req.query.address
+         })
+     })
+    // res.send({ 
+    //     forecast:'It is snowing',
+    //     location: "Philadelphia",
+    //     address: req.query.address
+    // })
+})
+
+app.get('/product', (req,res) => { 
+    if(!req.query.search) { 
+        return res.send({ 
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({ 
+        product: []
+    })
+})
+
+app.get('/help/*', (req,res) => { 
+    // res.send('Help article not found')
+    res.render('404', {
+        title: '404',
+        name: 'Ayush Agarwal',
+        errorMessage: 'Help article not  found.'
+    })
+})
+
+app.get('*', (req,res) => { 
+    // res.send('My 404 page')
+
+    res.render('404', {
+        title: '404',
+        name: 'Ayush Agarwal', 
+        errorMessage: 'Page not found.'
+    })
+})
+//app.com 
+//app.com/about
+//app.com/help
+
+app.listen(3000, () => { 
+    console.log('Server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+const forecast= (address,callback) => { 
+    const url ='https://samples.openweathermap.org/data/2.5/'+ address + '?id=524901&appid=b1b15e88fa797225412429c1c50c122a1'
+    request({ url,json:true},(error, { body} ) => { 
+        if(error){ 
+            callback('Unable to connect to weather service!', undefined)       
+         } else if(body.error){
+                    callback('Unable to find location.Try another search.',undefined)
+            
+                 } else{ 
+                    callback(undefined,{ 
+                        forecast:'It is currently ' + body.list[0].main.temp_max + ' degress out. There is a '+  body.list[0].main.pressure+ ' % chance of rain',
+                        latitude: body.list[0].main.temp_max  ,
+                        longitude: body.list[0].main.temp_max,
+                        description: body.list[0].weather[0].description
+                    })
+                 }
+    })
+} 
+
+module.exports = forecast
